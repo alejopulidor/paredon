e88a8c409f536b6153ba9a94bf9f46946249e9a0
Alejandro Pulido <alejandro@whaleandjaguar.co>
2021-05-13 18:22:43 -0500

    add header navbar and some styles

--- src/components/HeaderNavBar/HeaderNavBar.js
+++ src/components/HeaderNavBar/HeaderNavBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import styles from './HeaderNavBar.scss';
+import styles from './HeaderNavBar.module.scss';
 
 const HeaderNavBar = () => {
     return (
         <div>
-            Header
+            <nav class="navbar navbar-light justify-content-between">
+                <span className={styles["title"]}>El paredón de la vergüenza</span>
+                <form class="form-inline">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search" />
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                </form>
+            </nav>
         </div>
     )
 }
